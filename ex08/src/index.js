0d11d29ec3ab82b735af705383bac546296cd608
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-23 23:39:30 +0100

    Add files via upload

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,1 +1,14 @@
+class Person {
+    constructor(name, email, age) {
+        this.name = name
+    }
 
+
+    getPerson() {
+        return name;
+        return email;
+        return age;
+
+    }
+}
+module.exports = User;
