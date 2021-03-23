bf1a6865c2d9505db619deb973b4726becad0099
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-23 17:20:49 +0100

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,18 @@
-
+// Only change code below this line
+class Book {
+    constructor(title) {
+        this._title = title;
+    }
+    get Book() {
+        return this.title;
+    }
+    set Book(newTitle) {
+        this._title = newTitle
+    }
+}
+// Only change code above this line
+let littlePrince = new Book("The Little Prince"); // Change this line
+console.log(littlePrince.title); // The Little Prince
+littlePrince.title = new Book("Le Petit Prince") ; // Change this line
+console.log(littlePrince.title); // Le Petit Prince
+module.exports = Book;
