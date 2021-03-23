501c3a94ef3fda8bdc96dccdfb34703f481521da
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-23 23:10:42 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,28 @@
+// Only change code below this line
+class Pets extends Dog {
+    constructor(name, legs)
+    super(Pets)
 
+    }
+
+function walk() {
+    return numberOfLegs();
+}
+class Dog{
+
+}
+function bark() {
+    var dogSay = 
+}
+    
+    
+
+// Only change code above this line
+let dog = new Dog(name, legs);  // Change this line
+console.log(dog.walk());
+console.log(dog.bark());
+
+module.exports = {
+    Pets,
+    Dog
+}
