c0c6b3283f7f1faeeacad1b8099e3a42259e1613
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-23 23:25:41 +0100

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,10 @@
-
+function firstName (firstName) {
+    return String.toUpperCase()
+}
+function lastName (lastName) {
+    return String.toLowerCase()
+}
+console.log(firstName, lastName)
+exports.firstName=firstName;
+exports.lastName=lastName;
+module.exports = {firstName, lastName};
