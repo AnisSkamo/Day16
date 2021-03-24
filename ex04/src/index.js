5a474203f232946dd7afda3489cb98a54381f9d7
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-24 09:52:14 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,24 +1,27 @@
 // Only change code below this line
-class Pets extends Dog {
+class Pets {
     constructor(name, legs)
-    super(Pets)
+    this.name = name;
+    this.legs = legs;
 
+}
+
+walk(numberOfLegs) {
+    console.log('${this._name} is walking on ${this._legs}');
+}
+class Dog extends Pets {
+    constructor(name, legs) {
+        super(name);
+        this.legs = legs;
     }
 
-function walk() {
-    return numberOfLegs();
+        bark() {
+            let dogSay = "Pujdo says WUF-WUF";
+            return dogSay;
+        }
 }
-class Dog{
-
-}
-function bark() {
-    var dogSay = 
-}
-    
-    
-
 // Only change code above this line
-let dog = new Dog(name, legs);  // Change this line
+let dog = new Dog("Pujdo", 4);  // Change this line
 console.log(dog.walk());
 console.log(dog.bark());
 
